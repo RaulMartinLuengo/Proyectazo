8cfdde6d4b7a0647ac6af3684c0af8b06c4f143f
Jesús Manzaneque Iniesta <jesusmanzanequeiniesta@gmail.com>
2023-08-24 11:50:44 +0200

    Corrección final de errores.

--- app_bromas/src/App/App.jsx
+++ app_bromas/src/App/App.jsx
@@ -1,11 +1,3 @@
-/* eslint-disable react/react-in-jsx-scope */
-/* eslint-disable react/button-has-type */
-/* eslint-disable import/no-named-as-default */
-/* eslint-disable import/prefer-default-export */
-/* eslint-disable max-len */
-/* eslint-disbale jsx-a11y/click-events-have-key-events */
-/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-
 import React, { useState } from 'react';
 import './App.css';
 import {
@@ -23,15 +15,20 @@ import Busqueda from './Busqueda/Busqueda';
 import Cantidad from './Cantidad/Cantidad';
 import Chistes from './Chistes/Chistes';
 import {
-  getAmountJoke, getCategoryAmountJoke, getCategoryJoke, getCategoryLengthAmountJoke, getCategoryLengthJoke, getFilterJoke, getFlagsJoke, getLangJoke, getLengthJoke,
+  getAmountJoke, getCategoryAmountJoke, getCategoryJoke,
+  getCategoryLengthAmountJoke, getCategoryLengthJoke, getFilterJoke,
+  getFlagsJoke, getLangJoke, getLengthJoke,
 } from '../services/jokeApi';
 
-export function App() {
+export default function App() {
   const defaultJoke = 'No hay ningún chiste activo';
   // Hook para los chistes cuyo estado inicial será un objeto vacío
-  const [jokes, setJokes] = useState(defaultJoke);// definimos un hook que contendrá los chistes, además tiene una función seteadora para cambiar el estado del mismo.
+  // definimos un hook que contendrá los chistes, además tiene una función
+  // seteadora para cambiar el estado del mismo.
+  const [jokes, setJokes] = useState(defaultJoke);
   // como estado por defecto le pasamos la variable que está arriba definida
-  // funcion fetch parametrizada. Recibe como parámetro un valor numérico, el cual es pasado como prop en otra función llamada Cantidad
+  // funcion fetch parametrizada. Recibe como parámetro un valor numérico,
+  // el cual es pasado como prop en otra función llamada Cantidad
   // que se encuentra en el componente Cantidad
   const toggleAmount = (amount) => {
     async function fetchJokes() {
@@ -48,7 +45,8 @@ export function App() {
     }
     fetchJokes();
   };
-  // Esta función recibe como props los dos parámetros utilizados en las dos funciones anteriores, lo que permite realizar una petición más detallada del recurso a recuperar
+  // Esta función recibe como props los dos parámetros utilizados en las dos funciones anteriores,
+  // lo que permite realizar una petición más detallada del recurso a recuperar
   const toggleCategoryAmount = (categoryname, amount) => {
     async function fetchJokes() {
       const jokeCategory = await getCategoryAmountJoke(categoryname, amount);
@@ -56,8 +54,10 @@ export function App() {
     }
     fetchJokes();
   };
-  // En esta otra función, además de categorías, se solicita que la longitud del joke sea entre chistes cortos o largos
-  // las props son pasadas a través de la llamada al componente Categorias. Explicar bien la función de este componente
+  // En esta otra función, además de categorías, se solicita que
+  // la longitud del joke sea entre chistes cortos o largos
+  // las props son pasadas a través de la llamada al componente Categorias.
+  // Explicar bien la función de este componente
   const toggleCategoryLength = (categoryname, length) => {
     async function fetchJokes() {
       const jokeCategory = await getCategoryLengthJoke(categoryname, length);
@@ -65,7 +65,8 @@ export function App() {
     }
     fetchJokes();
   };
-  // aquí, tenemos la última función fetch, la cual es un mix de todas la anteriores, por lo que la búsqueda del recurso queda más personalizada
+  // aquí, tenemos la última función fetch, la cual es un mix de todas la anteriores,
+  // por lo que la búsqueda del recurso queda más personalizada
   const toggleCategoryLengthAmount = (categoryname, length, amount) => {
     async function fetchJokes() {
       const jokeCategory = await getCategoryLengthAmountJoke(categoryname, length, amount);
@@ -73,8 +74,10 @@ export function App() {
     }
     fetchJokes();
   };
-  // Con esta función, elegimos el idioma por el que nos decantamos a la hora de hacer la petición de los jokes a la api
-  // las props son pasadas a través de la llamada al componetne Idioma. Cuyo componente tiene una función que recibe el valor capturado
+  // Con esta función, elegimos el idioma por el que nos decantamos
+  // a la hora de hacer la petición de los jokes a la api
+  // las props son pasadas a través de la llamada al componetne Idioma.
+  // Cuyo componente tiene una función que recibe el valor capturado
   // en el evento onChage del select
   const toggleLanguage = (language) => {
     async function fetchJokes() {
@@ -83,8 +86,11 @@ export function App() {
     }
     fetchJokes();
   };
-  // Función que permite realizar una petición fetch parametrizada según la palabra introducida en un input
-  // la props es pasada en la llamada al componente búsqueda. El componente búsqueda tiene una función la cuál recibe como parámetro el valor que es capturado
+  // Función que permite realizar una petición fetch parametrizada
+  // según la palabra introducida en un input
+  // la props es pasada en la llamada al componente búsqueda.
+  // El componente búsqueda tiene una función la cuál
+  // recibe como parámetro el valor que es capturado
   // en el evento onkeyUp del input
   const toggleWord = (word) => {
     async function fetchJokes() {
@@ -93,7 +99,8 @@ export function App() {
     }
     fetchJokes();
   };
-  // Función parametrizada que realiza una petición fetch a una api externa, teniendo como filtro en la petición un valor de uno de los nombres de los atriburtos del
+  // Función parametrizada que realiza una petición fetch a una api externa,
+  // teniendo como filtro en la petición un valor de uno de los nombres de los atributos del
   // objeto del array de jokes.
   // como todas las demás, la props es lanzada en la llamada a su componente
   const toggleLength = (length) => {
@@ -103,7 +110,8 @@ export function App() {
     }
     fetchJokes();
   };
-  // Esta es una función de tipo centinela, con la cual se pretende capturar un valor booleano(true o false), para permitir realizar una función y otra dependiendo de su valor
+  // Esta es una función de tipo centinela, con la cual se pretende capturar un valor
+  // booleano(true o false), para permitir realizar una función y otra dependiendo de su valor
   // Se le pasan las props como en los demás, a través de la llamada a su componente (temática)
   const toggleFlags = (flag) => {
     async function fetchJokes() {
@@ -121,11 +129,18 @@ export function App() {
       <main>
         <div className="container">
           <div className="centrarya">
+            {/* eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions */}
             <nav onClick={handleJoke} onKeyDown={handleJoke} className="centrarya__nav">
-              {/* capturamos el evento onClick con el propósito de establecer un valor por defecto para cada vez que se selecciones un elemento del nav
-            así, cada vez que iniciemos un componente, nos aparecerá el mismo comentario, y con ello conseguimos que no se queden grabados los chistes anteriormente seleccionados en los otros componentes */}
+              {/* capturamos el evento onClick con el propósito de establecer un valor
+              por defecto para cada vez que se selecciones un elemento del nav
+            así, cada vez que iniciemos un componente,
+            nos aparecerá el mismo comentario, y con ello conseguimos
+            que no se queden grabados los chistes
+            anteriormente seleccionados en los otros componentes */}
               <ul className="todo-menu">
-                {/* Con cada uno de estos link le estamos diciendo que cuando se les haga click, seamos redirigidos a una página en concreto a través de la llamada a su componente, el cuál es definido como valor en el to */}
+                {/* Con cada uno de estos link le estamos diciendo que cuando se les haga click,
+                seamos redirigidos a una página en concreto a través de la llamada a su componente,
+                el cuál es definido como valor en el to */}
                 <li><Link to="/" className="centrarya__nav--link">Home</Link></li>
                 <li><Link to="/categorias" className="centrarya__nav--link">Categorias</Link></li>
                 <li><Link to="/tematica" className="centrarya__nav--link">Tematica</Link></li>
@@ -137,50 +152,62 @@ export function App() {
             </nav>
           </div>
           <div className="container2">
-            {/* Este Switch tiene como cometido la entrada en la búsqueda de una coincidencia establecida entre el link que hayamaos clickado y el path del Route definida */}
+            {/* Este Switch tiene como cometido la entrada en la búsqueda de una
+            coincidencia establecida entre el link que hayamaos clickado
+            y el path del Route definida */}
             <Switch>
               <Route path="/categorias">
-                {/* En las llamadas para renderizar el componente, le pasamos como  props el nombre de la función, es decir, la función que realiza el fetch se la estamos pasando al componente */}
-                {/* Como podemos comprobar, esta llamada es más compleja, debido a que se necesita pasar como props 4 funciones, ya que la parametrización de la petición puede encontrarse dentro de estas posibilidades */}
-                <Categorias toggleCategory={toggleCategory} toggleCategoryAmount={toggleCategoryAmount} toggleCategoryLength={toggleCategoryLength} toggleCategoryAmountLength={toggleCategoryLengthAmount} />
+                {/* En las llamadas para renderizar el componente, le pasamos como
+                props el nombre de la función, es decir, la función que realiza
+                el fetch se la estamos pasando al componente */}
+                {/* Como podemos comprobar, esta llamada es más compleja,
+                debido a que se necesita pasar como props 4 funciones,
+                ya que la parametrización de la petición
+                puede encontrarse dentro de estas posibilidades */}
+                <Categorias
+                  toggleCategory={toggleCategory}
+                  toggleCategoryAmount={toggleCategoryAmount}
+                  toggleCategoryLength={toggleCategoryLength}
+                  toggleCategoryAmountLength={toggleCategoryLengthAmount}
+                />
                 <div className="render__chiste">
                   <Chistes joke={jokes} />
-                  <button type="reset" className="button button--clean" onClick={handleJoke}>Limpiar Chistes</button>
+                  <button type="button" className="button button--clean" onClick={handleJoke}>Limpiar Chistes</button>
                 </div>
               </Route>
               <Route path="/tematica">
                 <Tematica toggleFlags={toggleFlags} />
                 <div className="render__chiste">
                   <Chistes joke={jokes} />
-                  <button type="reset" className="button button--clean" onClick={handleJoke}>Limpiar Chistes</button>
+                  <button type="button" className="button button--clean" onClick={handleJoke}>Limpiar Chistes</button>
                 </div>
               </Route>
               <Route path="/longitud">
                 <Longitud toggleLength={toggleLength} />
                 <div className="render__chiste">
                   <Chistes joke={jokes} />
-                  <button type="reset" className="button button--clean" onClick={handleJoke}>Limpiar Chistes</button>
+                  <button type="button" className="button button--clean" onClick={handleJoke}>Limpiar Chistes</button>
                 </div>
               </Route>
               <Route path="/idioma">
                 <Idioma toggleLanguage={toggleLanguage} />
                 <div className="render__chiste">
                   <Chistes joke={jokes} />
-                  <button type="reset" className="button button--clean" onClick={handleJoke}>Limpiar Chistes</button>
+                  <button type="button" className="button button--clean" onClick={handleJoke}>Limpiar Chistes</button>
                 </div>
               </Route>
               <Route path="/busqueda">
                 <Busqueda toggleWord={toggleWord} />
                 <div className="render__chiste">
                   <Chistes joke={jokes} />
-                  <button type="reset" className="button button--clean" onClick={handleJoke}>Limpiar Chistes</button>
+                  <button type="button" className="button button--clean" onClick={handleJoke}>Limpiar Chistes</button>
                 </div>
               </Route>
               <Route path="/cantidad">
                 <Cantidad toggleAmount={toggleAmount} />
                 <div className="render__chiste">
                   <Chistes joke={jokes} />
-                  <button type="reset" className="button button--clean" onClick={handleJoke}>Limpiar Chistes</button>
+                  <button type="button" className="button button--clean" onClick={handleJoke}>Limpiar Chistes</button>
                 </div>
               </Route>
               {/* Cuidado: El / siempre va al final del Switch */}
